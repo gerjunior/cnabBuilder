fa5e17f62267d63726a0de06d4bc5e114bbc34c5
Geraldo Silva <geraldojuni@hotmail.com>
2023-12-05 18:04:14 -0300

    refac: linting with prettier

--- cnabRows.js
+++ cnabRows.js
@@ -1,26 +1,41 @@
 'use strict';
-import path from 'path'
-import { readFile } from 'fs/promises'
-import { fileURLToPath } from 'url';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
 
-import yargs from 'yargs'
-import chalk from 'chalk'
+import yargs from 'yargs';
+import chalk from 'chalk';
 
 const optionsYargs = yargs(process.argv.slice(2))
   .usage('Uso: $0 [options]')
-  .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
-  .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
-  .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
-  .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
-  .argv;
+  .option('f', {
+    alias: 'from',
+    describe: 'posição inicial de pesquisa da linha do CNAB',
+    type: 'number',
+    demandOption: true,
+  })
+  .option('t', {
+    alias: 'to',
+    describe: 'posição final de pesquisa da linha do CNAB',
+    type: 'number',
+    demandOption: true,
+  })
+  .option('s', {
+    alias: 'segment',
+    describe: 'tipo de segmento',
+    choices: ['p', 'q', 'r'],
+    type: 'string',
+    demandOption: true,
+  })
+  .example(
+    '$0 -f 21 -t 34 -s p',
+    'destaca o segmento de acordo com os parâmetros informados',
+  ).argv;
 
+const { from, to, segment } = optionsYargs;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const file = path.resolve(`${__dirname}/cnabExample.rem`)
-
-const { from, to, segmento } = optionsYargs
-
-const sliceArrayPosition = (arr, ...positions) => [...arr].slice(...positions)
+const file = path.resolve(`${__dirname}/cnabExample.rem`);
 
 const messageLog = (segmento, segmentoType, from, to) => `
 ----- Cnab linha ${segmentoType} -----
@@ -32,42 +47,39 @@ posição to: ${chalk.inverse.bgBlack(to)}
 item isolado: ${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}
 
 item dentro da linha P: 
-  ${segmento.substring(0, from)}${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}${segmento.substring(to)}
+  ${segmento.substring(0, from)}${chalk.inverse.bgBlack(
+    segmento.substring(from - 1, to),
+  )}${segmento.substring(to)}
 
 ----- FIM ------
-`
+`;
 
-const log = console.log
-
-console.time('leitura Async')
+console.time('leitura Async');
 
 readFile(file, 'utf8')
-  .then(file => {
-    const cnabArray = file.split('\n')
+  .then((file) => {
+    const lines = file.split('\n');
+    const header = lines.slice(0, 2);
 
-    const cnabHeader = sliceArrayPosition(cnabArray, 0, 2)
+    const [p, q, r] = lines.slice(2, -2);
+    const tail = lines.slice(-2);
 
-    const [cnabBodySegmentoP, cnabBodySegmentoQ, cnabBodySegmentoR] = sliceArrayPosition(cnabArray, 2, -2)
-
-    const cnabTail = sliceArrayPosition(cnabArray, -2)
-
-    if (segmento === 'p') {
-      log(messageLog(cnabBodySegmentoP, 'P', from, to))
-      return
+    if (segment === 'p') {
+      console.log(messageLog(p, 'P', from, to));
+      return;
     }
 
-    if (segmento === 'q') {
-      log(messageLog(cnabBodySegmentoQ, 'Q', from, to))
-      return
+    if (segment === 'q') {
+      console.log(messageLog(q, 'Q', from, to));
+      return;
     }
 
-    if (segmento === 'r') {
-      log(messageLog(cnabBodySegmentoR, 'R', from, to))
-      return
+    if (segment === 'r') {
+      console.log(messageLog(r, 'R', from, to));
+      return;
     }
-
   })
-  .catch(error => {
-    console.log("🚀 ~ file: cnabRows.js ~ line 76 ~ error", error)
-  })
-console.timeEnd('leitura Async')
+  .catch((error) => {
+    console.error(error);
+  });
+console.timeEnd('leitura Async');
